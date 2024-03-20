04955f887195f697bff0a2ac6fdb3f4593f58b37
GalicIvan <ivangalic2004@gmail.com>
2024-03-20 14:53:25 +0100

    Finished 'project'

--- src/script.js
+++ src/script.js
@@ -1,4 +1,16 @@
 import './styles.css';
 
-const content = document.querySelector('.content')
-const allButtons = document.querySelectorAll('.button')
+let header = document.querySelector('.header')
+let content = document.querySelector('.content')
+
+let headText = document.createElement('p')
+headText.textContent = 'Barrique'
+headText.classList.add('head-text')
+header.appendChild(headText)
+
+
+let contentText = document.createElement('div')
+contentText.textContent = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum libero ducimus temporibus, cupiditate quod quae deserunt, voluptatem omnis debitis minima mollitia porro sit beatae ratione illo! Iusto asperiores rem eius?'
+contentText.classList.add('content-text')
+contentText.classList.add('head-text')
+content.appendChild(contentText)
